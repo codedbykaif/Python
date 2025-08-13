02d552307843974e624facf742383b692b55c7f1
codedbykaif <kaif_246048@saitm.ac.in>
2025-08-13 15:20:38 +0530

    Add files via upload

--- Python/Chapter 5/Lecture stuff/04_union_set.py
+++ Python/Chapter 5/Lecture stuff/04_union_set.py
@@ -3,3 +3,4 @@ s2 = {54, 33, 2, 1}
 print(s1.union(s2))
 print(s2.union(s1))
 print(s1.intersection(s2))
+print({2, 34}.issubset(s1))
---
+++ Python/Chapter 5/Practice set/01_problem.py
@@ -0,0 +1,8 @@
+words = {
+  "madad":      "help",
+  "bachao":     "save me",
+  "jaldi karo": "hurry up",
+  "shukriya":   "thankyou"
+}
+word = input("enter word you want meaning of: ")
+print(words[word])
