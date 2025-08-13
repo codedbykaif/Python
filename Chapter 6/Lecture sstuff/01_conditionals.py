13f0bfb677ee6042e1a21a6f548216bc79fb1c6a
codedbykaif <kaif_246048@saitm.ac.in>
2025-08-13 22:23:34 +0530

    Add files via upload

---
+++ Chapter 6/Lecture sstuff/01_conditionals.py
@@ -0,0 +1,10 @@
+a = int(input("Enter your age: "))
+
+if(a>18):
+  print("you are above the age of conscent")
+  print("Good for you")
+  
+else:
+  print("you are below the age of conscent")
+
+  print("End of program")
