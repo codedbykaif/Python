50b18ea2f218ff491fae2ae82f33239cc86a88d3
codedbykaif <mdkailfmailme@example.com>
2025-09-02 20:30:58 +0530

    Commit

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Hello world")
+print("This is written for github setup")
+print("The python is my first programming language learning in my 2nd year of my degree")
---
+++ nechallenge.py
@@ -0,0 +1,9 @@
+# Basic Python Program: Sum of first N natural numbers
+
+# Taking input from user
+n = int(input("Enter a number: "))
+
+# Formula method
+sum_n = n * (n + 1) // 2
+
+print("The sum of first", n, "natural numbers is:", sum_n)
