461eee1e086c01fd09486e7a7fc96f9de08ed434
codedbykaif <mdkailfmailme@example.com>
2025-09-02 21:22:46 +0530

    CoderKanhaiya

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ print("Hello world")
 print("This is written for github setup")
 print("The python is my first programming language learning in my 2nd year of my degree")
 
-print("kaif bkl")
+print("kanhaiya the coder")
+
+
+
